64e3628eac7707018c816b185af7e8ab6a9067c9
github username <github email address>
2019-01-16 07:56:59 -0500

    Update images_controller.rb
    cleaning up conflicting routes

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -20,7 +20,7 @@ class ImagesController < ApplicationController
     		end
     	end
 
-    	post '/account' do
+    	post '/mystyle' do
     		@image = Image.new(style: params["_style"], brand: params["brand"])
     		@image.user = current_user
         if params[:file]
@@ -31,59 +31,5 @@ class ImagesController < ApplicationController
           end
           @image.picture = filename
         end
-
-    		if @image.save
-    			redirect "/account/#{@image.id}"
-    		else
-    			redirect "/account/new"
-    		end
     	end
-
-    	get '/account/:id/edit' do
-    		if logged_in?
-    			@image = Image.find_by(id: params[:id])
-    			erb :'account/edit'
-    		else
-    			redirect '/'
-    		end
-    	end
-
-    	post '/account/:id' do
-    		@image = Image.find_by(id: params[:id])
-    		@image.update(style: params["_style"], brand: params["brand"])
-    		if params[:file]
-          filename = params[:file][:filename]
-          file = params[:file][:tempfile]
-          File.open("./public/images/#{filename}", 'wb') do |f|
-            f.write(file.read)
-          end
-          @image.picture = filename
         end
-        if @image.save
-    			redirect "/account/#{params[:id]}"
-    		else
-    			redirect "/account/#{params[:id]}/edit"
-    		end
-    	end
-
-    	get '/account/:id' do
-    		if logged_in?
-    			@image = Image.find_by(id: params[:id])
-    			@user = current_user
-    			erb :'account/show'
-    		else
-    			redirect '/'
-    		end
-    	end
-
-      delete '/account/:id/delete' do
-        @image = Image.find_by(id: params[:id])
-        if logged_in? && @image.user == current_user
-          @image.destroy
-          session[:message] = "You successfully deleted your image"
-          redirect '/account'
-        else
-          redirect '/account/#{@image.id}'
-        end
-      end
-end
